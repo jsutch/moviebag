cd66abf9add7e0fa079af86f3f624727f9655bcc
Jeff Sutch <jeff.sutch@acm.org>
2021-06-22 09:28:49 -0700

    added example config

---
+++ example_conf/creds.py
@@ -0,0 +1,8 @@
+mongouser = 'myRWMongoUser'
+mongopassword = 'RWMongoUserPassword'
+JWT_SECRET_KEY = "JWTSecretKey"
+MAIL_SERVER = "my.server.name"
+MAIL_PORT = "25"
+MAIL_PORT_TEST = "1025"
+MAIL_USERNAME = "myname@mymailrelay.com"
+MAIL_PASSWORD = ""
